a30b4037253ed2427a3a32e89d14be3376bba50d
Ajaykrishnan000 <136305504+Ajaykrishnan000@users.noreply.github.com>
2026-01-14 18:32:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const SHEET_URL =  "https://docs.google.com/spreadsheets/d/1mY2_sIyD8t2l3LR4ztLL3Cy-EAHG-Ej7vm01w90Ndww/edit?usp=sharing";
+const SHEET_URL =  "https://opensheet.elk.sh/1mY2_sIyD8t2l3LR4ztLL3Cy-EAHG-Ej7vm01w90Ndww/1";
 const phone = "919XXXXXXXXX"; // your WhatsApp number
 
 let allProducts = [];
